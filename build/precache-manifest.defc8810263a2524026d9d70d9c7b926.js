a9f983e4bf02714342f2d086bc276c9e97d3ebea
Elderick <erickoliveiraah@gmail.com>
2019-10-20 11:55:22 -0300

    Deploy front end

---
+++ build/precache-manifest.defc8810263a2524026d9d70d9c7b926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7bf69ddcdd0eb80aad129c6885fc0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a62005d6f901f4699b5",
+    "url": "/static/css/main.0db81bfe.chunk.css"
+  },
+  {
+    "revision": "beac17a57da0fad22e91",
+    "url": "/static/js/2.eeeb9865.chunk.js"
+  },
+  {
+    "revision": "6a62005d6f901f4699b5",
+    "url": "/static/js/main.a1560378.chunk.js"
+  },
+  {
+    "revision": "d92e7acfcbfa197868a5",
+    "url": "/static/js/runtime-main.d36c9b34.js"
+  }
+]);
